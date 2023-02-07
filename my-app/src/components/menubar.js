f3ce072c7cc4703d87ca314dbd8afb15b86ac582
Rona Wang <rona@mit.edu>
2023-02-07 17:26:08 -0500

    changed checkoff menu

--- my-app/src/components/menubar.js
+++ my-app/src/components/menubar.js
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { AppBar, Toolbar, Button } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Menu from "@mui/material/Menu";
@@ -6,6 +7,15 @@ import "./menubar.css";
 import { Link } from "react-router-dom";
 
 export default function MenuBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <nav className="header">
       <div className="nav-wrapper">
@@ -32,21 +42,39 @@ export default function MenuBar() {
                 <h4>Preference Form</h4>
               </a>
             </Button>
-            <Button variant="text" size="small" style={{ marginRight: 16 }}>
-              <a href="https://airtable.com/shr5s7HXraqZt8pj3">
-                <h4>Kitchen Checkoff</h4>
-              </a>
-            </Button>
-            <Button variant="text" size="small" style={{ marginRight: 16 }}>
-              <a href="https://airtable.com/shrzKhkThkd2zO9NF">
-                <h4>House Checkoff</h4>
-              </a>
-            </Button>
-            <Button variant="text" size="small" style={{ marginRight: 16 }}>
-              <a href="https://airtable.com/shrDCpzEMr5HKyBYW">
-                <h4>Makeup Checkoff</h4>
-              </a>
-            </Button>
+            <div>
+              <Button
+                variant="text"
+                size="small"
+                style={{ marginRight: 16 }}
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <h4>Checkoffs</h4>
+              </Button>
+
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <a href="https://airtable.com/shr5s7HXraqZt8pj3">Kitchen</a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <a href="https://airtable.com/shrzKhkThkd2zO9NF">House</a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <a href="https://airtable.com/shrDCpzEMr5HKyBYW">Makeup</a>
+                </MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
       </div>
